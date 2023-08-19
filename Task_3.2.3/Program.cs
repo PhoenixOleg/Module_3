95d47d6da0eedd2931a9f0d000bec19609c6995a
PhoenixOleg <metall_2000@mail.ru>
2023-08-19 23:15:12 +0300

    Добавьте файлы проекта.

---
+++ Task_3.2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_3_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Oleg";
+
+            Console.WriteLine(myName);
+            Console.ReadKey();
+        }
+    }
+}
